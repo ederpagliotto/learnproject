b4a751db31f34cf62b797cdc9b4733cb0695779a
Eder Pagliotto <ederpagliotto@gmail.com>
2023-06-04 18:40:53 +0100

    adding nav components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Home />
+      <About />
+      <Services />
+      <Contact />
     </div>
   );
 };
