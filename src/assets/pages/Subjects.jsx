a691296ae3935c8c106385ce23016d1dac80b0e2
Eder Pagliotto <ederpagliotto@gmail.com>
2023-06-07 17:16:03 +0100

    creating subjects component

---
+++ src/assets/pages/Subjects.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import codeIMG from './assets/images/codeIMG.jpg';
+import englishIMG from './assets/images/englishIMG.jpg';
+import { NavLink } from 'react-router-dom';
+
+const Subjects = () => {
+  return (
+    <div className="subjects">
+      <NavLink to="/programming" className="subject-img">
+        <div>
+          <h1>Programming</h1>
+          <p>The best tools to learn to code for free.</p>
+          <img src={codeIMG} alt="" />
+        </div>
+      </NavLink>
+      <NavLink to="/english" className="subject-img">
+        <div>
+          <h1>English</h1>
+          <p>The best tools to learn english for free.</p>
+          <img src={englishIMG} alt="" />
+        </div>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Subjects;
