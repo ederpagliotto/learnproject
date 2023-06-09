657be45c2f448011c111b45bf7f354e7965f2367
Eder Pagliotto <ederpagliotto@gmail.com>
2023-06-09 18:22:15 +0100

    creating NotFound page message!

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const NotFound = () => {
-  return <div>Error 404!</div>;
+  return (
+    <div>
+      <h1>Error 404! - Invalid Link!</h1>
+    </div>
+  );
 };
 
 export default NotFound;
